45ded2bfd31dd841a533f2dd07c8c9ed12eb7f88
Make Developer <makedeveloper@Pratiks-Mac-mini.local>
2026-01-08 18:16:30 +0530

    Refactored the destination component

--- app/destinations/page.tsx
+++ app/destinations/page.tsx
@@ -10,6 +10,7 @@ async function fetchDestinations() {
 
 export default async function HomePage() {
   const destinations = await fetchDestinations();
+  console.log(destinations);
   return (
     <>
       <Header />
@@ -20,27 +21,28 @@ export default async function HomePage() {
           </h1>
 
           <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {destinations.map((d: any) => (
+            {destinations.map((d: any,index:number) => (
               <li
-                key={d.id}
+                key={d?.id}
                 className="bg-white rounded-2xl border border-gray-200 shadow-sm hover:shadow-md transition p-6 flex flex-col items-center justify-center text-center"
               >
                 {/* Optional image */}
-                {d.image && (
-                  <img
-                    src={d.image}
-                    alt={d.title}
-                    className="w-full h-40 object-cover rounded-lg mb-4"
-                  />
-                )}
+                {d.thumbnailUrl
+                  && (
+                    <img
+                      src={"https://picsum.photos/200?random="+(index+1)}
+                      alt={d?.title}
+                      className="w-full h-40 object-cover rounded-lg mb-4"
+                    />
+                  )}
 
                 <h2 className="text-lg font-semibold text-gray-900 mb-2">
-                  {d.title}
+                  {d?.title}
                 </h2>
 
                 {d.description && (
                   <p className="text-gray-600 text-sm leading-relaxed">
-                    {d.description}
+                    {d?.description}
                   </p>
                 )}
               </li>
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images:{
-    domains:["images.unsplash.com","via.placeholder.com"],
+    domains:["images.unsplash.com","via.placeholder.com","picsum.photos"],
+    
   },
   /* config options here */ 
 };
